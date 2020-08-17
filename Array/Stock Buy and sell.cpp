2d6940547c9d91c28c062300bdaebdaf31c98ecd
Arpit Sachan <arpitsachan46@gmail.com>
2020-08-17 16:30:20 +0530

    Stock buy and sell update

--- Array/Stock Buy and sell.cpp
+++ Array/Stock Buy and sell.cpp
@@ -7,50 +7,31 @@ struct Interval
     int sell;
 };
 
-void stockBuySell(int a[], int n)
-{
-   
-   int i=0;
-   vector<Interval> v;
-   while(i<n-1){
-       int j=i;
-       int buy=i;
-       int l_min=a[i];
-       int l_max=0;
-       while(j<n && a[i]>=a[j]){
-          if(l_min>=a[j]){
-              l_min=a[j];
-              buy=j;
-          }
-           j++;
-       }
-       int k=i+1;
-       int sell=buy;
-       while(k<n && a[k]>=a[k-1])
-       {
-           if(l_max<=a[k]){
-               l_max=a[k];
-               sell=k;
-           }
-           k++;
-       }
-      if(a[buy]!=a[sell]  &&buy<sell){
-      v.push_back({buy, sell});
-          
-      }
-      if(a[buy]==a[sell])
-      {
-          i++;
-      }else
-        i =sell+1;
-   }
-   if(!v.empty())
-   for(int i=0;i<v.size();i++){
-      
-       cout<<"("<<v[i].buy<<" "<<v[i].sell<<")"<<" ";
-   }else cout<<"No Profit";
+int buy=0;
+    int sell=1;
     
-}
+    int profit=0;
+    for(int i=1;i<n;i++){
+        if(p[i]>p[i-1]){
+            profit+=p[i]-p[i-1];
+        }
+    }
+    if(profit>0){
+    while(buy<n && sell<n){
+        while(sell<n && p[sell]>=p[sell-1]){
+            sell++;
+        }
+        if(buy!=sell-1){
+        cout<<"("<<buy<<" "<<sell-1<<")"<<" ";}
+
+        buy=sell;
+        while(buy<n-1 && p[buy+1]<=p[buy]) buy++;
+        sell=buy+1;
+    }
+    }
+    else{
+        cout<<"No Profit";
+    }
 int main()
 {   
     
